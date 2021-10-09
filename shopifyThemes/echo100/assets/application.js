5453efd69e401b966a7c58623c4ac486390bbb63
DreamEcho100 <maze6572198@gmail.com>
2021-10-09 04:00:32 +0200

    - In shopifyThemes/echo100: Adding a product Filters (Sorting) to collection.

--- shopifyThemes/echo100/assets/application.js
+++ shopifyThemes/echo100/assets/application.js
@@ -1,1 +1,9 @@
 // Put your application javascript here
+const sort_bySelect = document.getElementById('sort_by');
+
+sort_bySelect.addEventListener('change', (event) => {
+	const url = new URL(window.location.href);
+	url.searchParams.set('sort_by', event.currentTarget.value);
+
+	window.location = url.href;
+});
